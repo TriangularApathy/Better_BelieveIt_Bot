00c7f8a293f801d509e1ebd2f9363f6d76f833d7
Silas Walker-Tjepkes <services@pmgsolutions.onmicrosoft.com>
2024-06-02 13:49:12 -0700

    add services

---
+++ src/Services/DiscordStartupService.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Better_BelieveIt_Bot.Utility;
+
+namespace Better_BelieveIt_Bot.Services {
+    internal class DiscordStartupService : IHostedService {
+        private readonly DiscordSocketClient _client = null!;
+        private readonly IConfiguration _config = null!;
+        private readonly ILogger<DiscordSocketClient> _logger = null!;
+
+        public DiscordStartupService(DiscordSocketClient client, IConfiguration config, ILogger<DiscordSocketClient> logger) {
+            _client = client;
+            _config = config;
+            _logger = logger;
+
+            Console.WriteLine("I made it to 'DiscordStartupService'");
+            Console.WriteLine($"Client: {_client}");
+            Console.WriteLine($"Config: {_config}");
+            Console.WriteLine($"Logger: {_logger}");
+
+            _client.Log += message => LogHandler.OnLogAsync(_logger, message);
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken) {
+            Console.WriteLine("I made it to 'StartAsync'");
+            await _client.LoginAsync(TokenType.Bot, _config["BotToken"]);
+            await _client.StartAsync();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken) {
+            await _client.LogoutAsync();
+            await _client.StopAsync();
+        }
+    }
+}
---
+++ src/Services/InteractionHandlingService.cs
@@ -0,0 +1,63 @@
+﻿using Better_BelieveIt_Bot.Utility;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Better_BelieveIt_Bot.Services {
+    internal class InteractionHandlingService : IHostedService {
+        private readonly DiscordSocketClient _client;
+        private readonly InteractionService _interaction;
+        private readonly IServiceProvider _services;
+        private readonly IConfiguration _config;
+        private readonly ILogger<InteractionService> _logger;
+
+        public InteractionHandlingService(
+            DiscordSocketClient client,
+            InteractionService interaction,
+            IServiceProvider services,
+            IConfiguration config,
+            ILogger<InteractionService> logger) {
+            _client = client;
+            _interaction = interaction;
+            _services = services;
+            _config = config;
+            _logger = logger;
+
+            Console.WriteLine("I made it into 'InteractionHandlingService'");
+
+            _interaction.Log += message => LogHandler.OnLogAsync(_logger, message);
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken) {
+            Console.WriteLine("I made it to 'StartAsync' in the 'InteractionHandlingService'");
+            _client.Ready += () => _interaction.RegisterCommandsGloballyAsync(true);
+            _client.InteractionCreated += OnInteractionAsync;
+
+            //await _interaction.AddModulesAsync(Assembly.GetEntryAssembly(), _servies);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            _interaction.Dispose();
+            return Task.CompletedTask;
+        }
+
+        private async Task OnInteractionAsync(SocketInteraction interaction) {
+            try {
+                var context = new SocketInteractionContext(_client, interaction);
+                var result = await _interaction.ExecuteCommandAsync(context, _services);
+
+                if (!result.IsSuccess)
+                    await context.Channel.SendMessageAsync(result.ToString());
+            }
+
+            catch {
+                if (interaction.Type == Discord.InteractionType.ApplicationCommand) {
+                    await interaction.GetOriginalResponseAsync()
+                        .ContinueWith(message => message.Result.DeleteAsync());
+                }
+            }
+        }
+    }
+}
