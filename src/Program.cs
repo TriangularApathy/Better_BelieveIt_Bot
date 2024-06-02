237cfb4d4d0117e829a7930a0795cf534ed6ff77
Silas Walker-Tjepkes <services@pmgsolutions.onmicrosoft.com>
2024-06-02 13:48:55 -0700

    refactored code

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿using Better_BelieveIt_Bot.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.WebSocket;
+using Discord.Interactions;
+using Microsoft.Extensions.Logging;
+
+namespace Better_BelieveIt_Bot {
+    internal class Program {
+        public static async Task Main(string[] args) {
+            using IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config => {
+                    config.AddUserSecrets<Program>();
+                })
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.SetMinimumLevel(LogLevel.Debug);
+                })
+                .ConfigureServices(services => {
+                    // add all services here
+                    services.AddSingleton<DiscordSocketClient>();
+                    services.AddSingleton<InteractionService>();
+                    services.AddSingleton<InteractionHandlingService>();
+                    services.AddSingleton<DiscordStartupService>();
+                })
+                .Build();
+            await host.RunAsync();
+        }
+    }
+}
