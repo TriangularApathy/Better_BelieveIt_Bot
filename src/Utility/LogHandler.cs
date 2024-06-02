1088ffeb45454df373a575c4d094663e0abd7893
Silas Walker-Tjepkes <services@pmgsolutions.onmicrosoft.com>
2024-06-02 13:49:46 -0700

    add utilities

---
+++ src/Utility/LogHandler.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Better_BelieveIt_Bot.Utility {
+    internal static class LogHandler {
+        public static Task OnLogAsync(ILogger logger, LogMessage message) {
+            switch (message.Severity) {
+                case LogSeverity.Debug:
+                    logger.LogInformation("{Message}",message.ToString());
+                    break;
+                case LogSeverity.Verbose:
+                    logger.LogInformation("{Message}", message.ToString());
+                    break;
+                case LogSeverity.Info:
+                    logger.LogInformation("{Message}", message.ToString());
+                    break;
+                case LogSeverity.Warning:
+                    logger.LogWarning("{Message}", message.ToString());
+                    break;
+                case LogSeverity.Error:
+                    logger.LogError("{Message}", message.ToString());
+                    break;
+                case LogSeverity.Critical:
+                    logger.LogCritical("{Message}", message.ToString());
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
