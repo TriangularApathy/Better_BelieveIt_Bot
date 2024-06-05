148addbf2c95a5d54e99f129ae365cc713303e84
Silas Walker-Tjepkes <services@pmgsolutions.onmicrosoft.com>
2024-06-05 14:51:52 -0700

    add module for simple commands

---
+++ src/Modules/SimpleCommandsModule.cs
@@ -0,0 +1,17 @@
+﻿using Discord.Interactions;
+
+namespace Better_BelieveIt_Bot.Modules {
+    public class SimpleCommandsModule : InteractionModuleBase<SocketInteractionContext> {
+
+        [SlashCommand("ping", "Replies with 'Pong'")]
+        public async Task Ping() {
+            await RespondAsync("Pong!", ephemeral: true);
+        }
+
+        [SlashCommand("math_add", "Add two integers together.")]
+        public async Task MathAdd(int a, int b) {
+            long c = a + b;
+            await RespondAsync($"The sum of [{a}] and [{b}] is [{c}]");
+        }
+    }
+}
