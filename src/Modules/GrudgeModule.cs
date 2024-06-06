19441a77e13a8c10db319e8b8495e1cee06c8999
Silas Walker-Tjepkes <saladecroutons@gmail.com>
2024-06-06 13:39:32 -0700

    begin request subcommand

--- src/Modules/GrudgeModule.cs
+++ src/Modules/GrudgeModule.cs
@@ -3,7 +3,20 @@
 namespace Better_BelieveIt_Bot.src.Modules {
     [Group("grudges", "Commands to manage server grudges")]
     public class GrudgeModule : InteractionModuleBase<SocketInteractionContext> {
-        [SlashCommand("add", "Add a grudge to the fabled lis t of Grudges.")]
+        [SlashCommand("request", "Submit a Grudge to be reviewed.")]
+        public async Task RequestSubcommand(
+            [Summary(description: "The title of the grudge ex: ({user}-{user} honor grudge)")] string title,
+            [Summary(description: "The user(s) that was/were wronged.")] string wronged,
+            [Summary(description: "The user(s) that committed the Wrongdoing.")] string[] assailants,
+            [Summary(description: "The Wrongdoing deserving of an eternal grudge. ex: (Unrightful banning of *User* form *server*.)")] string wrongdoing,
+            [Summary(description: "The date of the Wrongdoing ex: (01/01/2024)")] DateTime date,
+            [Summary(description: "The action(s) that must take place in order to remove the grudge. ex: (Public apology)")] string[] terms) {
+
+        }
+
+        /*
+
+        [SlashCommand("add", "Add a grudge to the fabled list of Grudges.")]
         public async Task AddSubcommand() {
             // Add to group db or separate?
             // Newly added grudge should be sent to quarantine
@@ -17,11 +30,6 @@ namespace Better_BelieveIt_Bot.src.Modules {
         [SlashCommand("list", "Lists all confirmed Grudges.")]
         public async Task ListSubcommand() {
             // List only for the server in which it was called
-        }
-
-        [SlashCommand("request", "Submit a Grudge to be reviewed.")]
-        public async Task RequestSubcommand(string title, string wronged, string[] assailants, string wrongdoing, DateTime date, string terms) {
-
         }
 
         [SlashCommand("confirm", "Allows a Book-Holder to confirm requested Grudges.")]
@@ -42,5 +50,7 @@ namespace Better_BelieveIt_Bot.src.Modules {
 
         // dbcontext?
         // separate guilds?
+
+        */
     }
 }
